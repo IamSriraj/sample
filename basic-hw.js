d3a9d5705b2e0e5c87a5b0096124010444bf5998
IamSriraj <sri.uideveloper@gmail.com>
2016-08-11 11:02:58 -0500

    Update basic-hw.js

--- basic-hw.js
+++ basic-hw.js
@@ -61,4 +61,70 @@
     document.getElementById("greeting")
   );
   
+ /*
+ With the below example I would like to show 
+ how to create click events
+ */ 
+
+//To provide a component with a click handler function,
+//We need to pass it as an argument to onClick property
+
+class HelloWorld extends React.Component {
+  linkClicked(event) {
+    console.log(event);
+  }
+  render() {
+    return (
+      <div id="two">
+       <h1>Hello {this.props.name}</h1>
+       <a href="#" id="hit" onClick={this.linkClicked}>Hit me</a>
+      </div> 
+     );
+  }
+}
+
+ React.render(
+   <HelloWorld name="sri" />,
+   document.getElementById("greeting")
+ );
+
+/*
+I defined linkClicked method that logs the event with above example.
+Now with below example I would like to show how to change,
+content with click event.
+*/
+
+class HelloWorld extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {clicked: false};
+    this.linkClicked = this.linkClicked.bind(this);
+  }
   
+  linkClicked(event) {
+    this.setState({clicked: true});
+  }
+  
+  render() {
+    if (this.state.clicked === false) {
+     return (
+      <div id="two">
+       <h1>Hello {this.props.name}</h1>
+       <a href="#" id="hit" onClick={this.linkClicked}>Hit me</a>
+      </div> 
+     );
+    } else {
+      return (
+       <div id="two">
+        <h1>Hello {this.props.name}</h1>
+        <span>You hit me!</span>
+       </div> 
+      );
+    }
+  }
+}
+
+React.render(
+  <HelloWorld name="sri" />,
+  document.getElementById("greeting")
+ );
