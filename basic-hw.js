bf832ce9b324fbce30cb7b424179d4fc1512b0e8
IamSriraj <sri.uideveloper@gmail.com>
2016-08-10 22:22:28 -0500

    Update basic-hw.js

--- basic-hw.js
+++ basic-hw.js
@@ -3,18 +3,62 @@
  Hello world looks as follows:
  */
 
-class HelloWorld extends React.Component {
+ class HelloWorld extends React.Component {
     render() {
       return (
         <h1>Hello World!</h1>
       );
     }
-  }
+ }
   
-  //To render the component use React.render method:
+ //To render the component use React.render method:
   
   React.render(
     <HelloWorld />,
     document.getElementById("greeting")
   );
     
+//The below example shows how to render two tags:
+
+ class HelloWorld extends React.Component {
+    render() {
+      return (
+       <div id="two">
+         <h1>Hello World!</h1>
+         <a href="#" id="hit">Hit Me!</a>
+       </div> 
+      );
+    }
+ }
+ 
+ //To render the component use React.render method:
+  
+  React.render(
+    <HelloWorld />,
+    document.getElementById("greeting")
+  );
+  
+  /*
+  The below example shows how to render component with argument.
+  I am taking a name instead of world.
+  */
+
+ class HelloWorld extends React.Component {
+    render() {
+      return (
+       <div id="two">
+         <h1>Hello {this.props.name}</h1>
+         <a href="#" id="hit">Hit Me!</a>
+       </div> 
+      );
+    }
+ }
+ 
+ //To render the component use React.render method:
+  
+  React.render(
+    <HelloWorld name="sri" />,
+    document.getElementById("greeting")
+  );
+  
+  
